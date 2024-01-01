109e3db97d63e56e347ccb4f5a9a3b37e387c2bc
Michele De Stefano <--->
2024-01-01 09:49:55 +0100

    (checkpoint) Start writing the code for moving a servo motor

---
+++ move_servo/move_servo.ino
@@ -0,0 +1,14 @@
+/**
+ * This is project n. 5 into the Arduino starter kit manual.
+ *
+ * Author: Michele De Stefano
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
